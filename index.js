4904f655ded81dc430797be0b23f4febcb13c394
inknode <66970553+inknode@users.noreply.github.com>
2020-06-16 19:07:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const searchForm = document.querySelector('.search-loaction');
-const cityValue = document.querySelector('.search-loaction input');
-const cityName = document.querySelector('.city-name p');
-const cardBody = document.querySelector('.card-body');
-const timeImage = document.querySelector('.card-top img');
-const cardInfo = document.querySelector('.back-card');
+const searchForm = document.querySelector(".search-loaction");
+const cityValue = document.querySelector(".search-loaction input");
+const cityName = document.querySelector(".city-name p");
+const cardBody = document.querySelector(".card-body");
+const timeImage = document.querySelector(".card-top img");
+const cardInfo = document.querySelector(".back-card");
 
-const spitOutCelcius = (kelvin) => {
-    celcius = Math.round(kelvin - 273.15);
-    return celcius;
-}
-const isDayTime = (icon) => {
-    if (icon.includes('d')) { return true }
-    else { return false }
-}
+const spitOutCelcius = (kelvin) => Math.round(kelvin - 273.15);
+const isDayTime = (icon) => (icon.includes("d") ? true : false);
+
 updateWeatherApp = (city) => {
-    console.log(city);
-    const imageName = city.weather[0].icon;
-    const iconSrc = `http://openweathermap.org/img/wn/${imageName}@2x.png`
-    cityName.textContent = city.name;
-    cardBody.innerHTML = `
+  console.log(city);
+  const imageName = city.weather[0].icon;
+  const iconSrc = `http://openweathermap.org/img/wn/${imageName}@2x.png`;
+  cityName.textContent = city.name;
+  cardBody.innerHTML = `
     <div class="card-mid row">
             <div class="col-8 text-center temp">
               <span>${spitOutCelcius(city.main.temp)}&deg;C</span>
@@ -44,44 +39,38 @@ updateWeatherApp = (city) => {
             </div>
           </div>
     `;
-    if (isDayTime(imageName)) {
-        console.log('day');
-        timeImage.setAttribute('src', 'img/day_image.svg');
-        if (cityName.classList.contains('text-white')) {
-            cityName.classList.remove('text-white');
-        } else {
-            cityName.classList.add('text-black');
-        }
+  if (isDayTime(imageName)) {
+    timeImage.setAttribute("src", "img/day_image.svg");
+    console.log("day");
 
+    if (cityName.classList.contains("text-white")) {
+      cityName.classList.remove("text-white");
     } else {
-        console.log('night');
-        timeImage.setAttribute('src', 'img/night_image.svg');
-        if (cityName.classList.contains('text-black')) {
-            cityName.classList.remove('text-black');
-        } else {
-            cityName.classList.add('text-white');
-        }
-
+      cityName.classList.add("text-black");
+      timeImage.setAttribute("src", "img/night_image.svg");
+      console.log("night");
     }
+  } else if (cityName.classList.contains("text-black")) {
+    cityName.classList.remove("text-black");
+  } else {
+    cityName.classList.add("text-white");
+  }
 
-    cardInfo.classList.remove('d-none');
-}
-
-
+  cardInfo.classList.remove("d-none");
+};
 
 //add an event listner to the form
-searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const citySearched = cityValue.value.trim();
-    console.log(citySearched);
-    searchForm.reset();
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const citySearched = cityValue.value.trim();
+  console.log(citySearched);
+  searchForm.reset();
 
-    requestCity(citySearched)
-        .then((data) => {
-            updateWeatherApp(data);
-        })
-        .catch((error) => { console.log(error) })
-
-
-
-})
+  requestCity(citySearched)
+    .then((data) => {
+      updateWeatherApp(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
